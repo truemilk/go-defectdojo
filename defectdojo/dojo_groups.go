9c3140f23b23af314eabef146e2c721b623c3776
Ivan Giacomelli <ivan@nopsled.it>
2022-01-26 17:02:06 +0200

    Added product_types

---
+++ defectdojo/dojo_groups.go
@@ -0,0 +1,15 @@
+package defectdojo
+
+type DojoGroup struct {
+	Id          *int    `json:"id,omitempty"`
+	Name        *string `json:"name,omitempty"`
+	Description *string `json:"description,omitempty"`
+	Users       []*int  `json:"users,omitempty"`
+}
+
+type DojoGroups struct {
+	Count    *int         `json:"count,omitempty"`
+	Next     *string      `json:"next,omitempty"`
+	Previous *string      `json:"previous,omitempty"`
+	Results  []*DojoGroup `json:"results,omitempty"`
+}
